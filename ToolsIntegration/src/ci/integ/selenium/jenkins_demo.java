bd3603038866a6b4f569591db424a70fd666f00d
Admin <Admin@192.168.0.7>
2020-07-21 22:48:20 +0530

    Initial Commit

---
+++ ToolsIntegration/src/ci/integ/selenium/jenkins_demo.java
@@ -0,0 +1,30 @@
+package ci.integ.selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+ 
+public class jenkins_demo
+{
+ 
+@Test
+public void testGoogleSearch(){
+ 
+System.setProperty("webdriver.chrome.driver", "lib\\chromedriver.exe");
+WebDriver driver =new ChromeDriver();
+//it will open the goggle page
+driver.get("http://google.in"); 
+//we expect the title “Google “ should be present 
+String Expectedtitle = "Google";
+//it will fetch the actual title 
+String Actualtitle = driver.getTitle();
+System.out.println("Before Assetion " + Expectedtitle + Actualtitle);
+//it will compare actual title and expected title
+Assert.assertEquals(Actualtitle, Expectedtitle);
+//print out the result
+System.out.println("After Assertion " + Expectedtitle + Actualtitle + " Title matched ");
+driver.close();
+ }
+}
